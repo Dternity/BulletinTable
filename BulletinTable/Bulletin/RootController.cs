e4d008f8766079c4c34a3d0363b301ea2bb2d48f
Richard Miafer <richard.brasetvik@hotmail.com>
2021-11-10 16:05:54 +0100

    [*] RootController testing

--- BulletinTable/Bulletin/RootController.cs
+++ BulletinTable/Bulletin/RootController.cs
@@ -6,12 +6,12 @@
 
         public ArticleController? ArticleController { get; private set; }
 
-        
         public void Init()
         {
             _inst.ArticleController = new ArticleController();
 
-            var article = new Article { Title = "ArticleTest!", Content = "This true content!", GUID = Guid.NewGuid() };
+            var article = new Article { Title = "ArticleTest!", Content = @"<a href='http://Funn.no/'>" +
+            "<img src='images/BlazorHelpWebsite.gif' /></a>", GUID = Guid.NewGuid() };
             _inst.ArticleController.Add(article);
 
         }
